e63cf222cb02118e7dd13d48702cdffee2ca6754
Ansar Smagul <me@anfragment.net>
2026-06-04 17:14:52 +0500

    Implement key creation and export on Windows

---
+++ createkey.go
@@ -0,0 +1,5 @@
+package main
+
+// func createKey(name string) (key crypto.Signer, err error)
+// func loadKey(name string) (crypto.Signer, error)
+// func deleteKey(name string) error
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/anfragment/key-protecc
+
+go 1.26.3
+
+require golang.org/x/sys v0.45.0
